cc2240ceb4d5c4aa35a35c6b0a72812f70cea5b0
Kali <hank1313@aol.com>
2022-10-19 12:31:57 -0400

    Fixes kmsg device, mkdir issues

--- mkrootfs.sh
+++ mkrootfs.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 cd rootfs;
-mkdir -p {dev, proc, sys, tmp, mnt, bin, usr, usr/lib, lib, etc, bin}
+mkdir -p dev
+mkdir -p proc
+mkdir -p sys
+mkdir -p tmp 
+mkdir -p mnt
+mkdir -p usr
+mkdir -p usr/lib
+mkdir -p lib
+mkdir -p etc
+mkdir -p bin
+
+sudo mknod dev/kmsg c 1 11;
 sudo mknod dev/console c 5 1;
 sudo mknod dev/ram0 b 1 0;
 find . -print0 | cpio --null --create --verbose --format=newc | gzip --best > ../fuzzcan-initramfs.cpio.gz
